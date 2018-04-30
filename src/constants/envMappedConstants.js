376d9d2ef798c332794ecd10ba8249c7b700df52
Theodore Li <theol88@gmail.com>
2018-04-29 21:25:27 -0700

    Add breaks to switch statement. Duh

--- src/constants/envMappedConstants.js
+++ src/constants/envMappedConstants.js
@@ -11,9 +11,11 @@ switch (process.env.NODE_ENV) {
   case ENV_PROD:
     ROUTING_BASE_PATH_MAPPED = apiConsts.ROUTING_BASE_SUB_PATH_PROD
     AUTH_QUERY_PARAMS_MAPPED = apiConsts.AUTH_QUERY_PARAMS_PROD;
+    break;
   case ENV_DEV:
     ROUTING_BASE_PATH_MAPPED = apiConsts.ROUTING_BASE_SUB_PATH_DEV
     AUTH_QUERY_PARAMS_MAPPED = apiConsts.AUTH_QUERY_PARAMS_DEV;
+    break;
   default:
     console.log('Warning, undefined case for process.env');
 }
