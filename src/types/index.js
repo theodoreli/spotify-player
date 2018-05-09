fe9c70785649476866a72ad84f71cc4117f6fc68
Theodore Li <theol88@gmail.com>
2018-05-09 00:20:30 -0700

    Remove comments in types file

--- src/types/index.js
+++ src/types/index.js
@@ -6,7 +6,3 @@ export const Track = new ObjectModel({
   audioSrc: String,
   trackName: String,
 });
-
-// in our action where we get this data,
-// make a util that parses the json into this model
-
